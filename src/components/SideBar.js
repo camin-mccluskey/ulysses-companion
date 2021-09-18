6ba040afca7d098014b930adc1402422c09ead7c
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2021-09-18 01:09:40 +0100

    correctly link joyceproject.com

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -27,7 +27,7 @@ const SideBar = ({chapters, currentChapter, selectChapter}) => {
         {aboutOpen &&
           <div className="text-sm">
           <p>This site is online edition of James Joyce’s Ulysses that comprehensively assists readers of this great modern novel, as they read.</p>
-          <p>The content for this site has been pulled from <a href="www.joyceproject.com">The Joyce Project</a> and is distributed here in accordance with their fair use <a href="https://www.joyceproject.com/pages/fairuse.htm">policy</a></p>
+          <p>The content for this site has been pulled from <a href="https://www.joyceproject.com">The Joyce Project</a> and is distributed here in accordance with their fair use <a href="https://www.joyceproject.com/pages/fairuse.htm">policy</a></p>
           <p>The purpose of this site is to improve upon the user experience of the excellent Joyce Project by opening notes in a sidebar and showing users which notes they have opened.</p>
           </div>
         }
