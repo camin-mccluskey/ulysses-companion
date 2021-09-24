cf419f44c12664d5772af67136082b3fd4583d37
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2021-09-24 08:57:33 +0100

    Clean up unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import data from './data';
 
 import SideBar from './components/SideBar';
@@ -7,8 +7,6 @@ import DetailBar from './components/DetailBar';
 
 
 function App() {
-  // const [currentChapter, setChapter] = useState(data.chapters[0]);
-  // const [noteId, setNoteId] = useState(null);
 
   return (
     <div className="flex w-screen h-screen p-2 gap-x-4">
